8d0455edd977a5bccfce627ee248a6eb6c6e26b8
byteveil <152559788+byteveil@users.noreply.github.com>
2023-12-05 11:26:01 -0800

    initial

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+using RobloxFiles;
+
+namespace rbxml_convert
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConvertButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Roblox Files|*.rbxm;*.rbxmx|All Files|*.*",
+                Title = "Select a Roblox file"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+                FilePathTextBox.Text = filePath;
+
+                ResultTextBlock.Text = "Processing...";
+                RobloxFile robloxFile = RobloxFile.Open(filePath);
+
+                switch (robloxFile)
+                {
+                    case BinaryRobloxFile:
+                        {
+                            ResultTextBlock.Text = "Converting from BinaryRobloxFile";
+                            RobloxFile xmlResult = new XmlRobloxFile();
+                            foreach (Instance robloxInstance in robloxFile.GetChildren())
+                            {
+                                robloxInstance.Parent = xmlResult;
+                            }
+                            string savePath = filePath.Replace(".rbxm", ".rbxmx");
+                            xmlResult.Save(savePath);
+                            ResultTextBlock.Text = $"Conversion complete. File saved at: {savePath}";
+                            break;
+                        }
+
+                    case XmlRobloxFile:
+                        {
+                            ResultTextBlock.Text = "Converting from Xml";
+                            RobloxFile binaryResult = new BinaryRobloxFile();
+                            foreach (Instance robloxInstance in robloxFile.GetChildren())
+                            {
+                                robloxInstance.Parent = binaryResult;
+                            }
+                            string savePath = filePath.Replace(".rbxmx", ".rbxm");
+                            binaryResult.Save(savePath);
+                            ResultTextBlock.Text = $"Conversion complete. File saved at: {savePath}";
+                            break;
+                        }
+                }
+            }
+            else
+            {
+                ResultTextBlock.Text = "File selection canceled.";
+            }
+        }
+    }
+}
